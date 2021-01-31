e90fcf7a716ebf1d38b3ff16aafd0ac27007c6d8
dinesh086 <dineshkumarmca086@gmail.com>
2021-01-31 18:24:25 +0530

    cart-31st-Jan-21

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Productsn from "../components/ProductsPage/Products";
+import Hero from "../components/Hero";
+import productsBcg from "../images/productsBcg.jpeg";
+import Products from "../components/ProductsPage/Products";
 
 export default function ProductsPage() {
   return (
     <>
-      <h1>Hello From Products Page</h1>
+      <Hero img={productsBcg} />
+      <Products />
     </>
   );
 }
